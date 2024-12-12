88248d97b0815e9fc7d5be5f60a5c520b1613311
Ben Gardiner <bengardiner18@gmail.com>
2024-12-12 16:37:53 -0500

    removing stats and port from landing page

--- app/(pages)/(landing-page)/page.tsx
+++ app/(pages)/(landing-page)/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
-      <Stats />
-      <Portfolio />
+      {/* <Stats /> */}
+      {/* <Portfolio /> */}
       <Investment />
     </main>
   );
